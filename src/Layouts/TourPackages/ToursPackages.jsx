8ad4f050f3b873ebab9d61a6d8340f3211a3bdcd
George <shegerageorgy@gmail.com>
2024-10-07 12:56:06 +0300

    Second Merge

--- src/Layouts/TourPackages/ToursPackages.jsx
+++ src/Layouts/TourPackages/ToursPackages.jsx
@@ -1,25 +1,44 @@
 import React, { useContext, useEffect, useRef, useState } from "react";
 import style from "./ToursPackages.module.scss";
+import { SectionRefsContext } from "../../Providers/SectionRefsContext";
+import { useDispatch, useSelector } from "react-redux";
+import { selectTourCards, initTourCards } from "../../Slices/tours/toursSlice";
+import TourCard from "../../Components/TourCard/TourCard";
 
 export default function ToursPackages({ className }) {
-  const [offsetY, setOffsetY] = useState(0);
+  const { setIntroSection } = useContext(SectionRefsContext);
+  const toursSection = useRef(null);
+  const tourCards = useSelector(selectTourCards);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [cardsCount, setCardsCount] = useState(8);
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const header = document.getElementsByTagName("header");
-    if (header) {
-      setOffsetY(getComputedStyle(header[0]).height);
-    }
-  }, [document.getElementsByTagName("header")]);
+    setIntroSection(toursSection.current);
+  }, []);
+
+  useEffect(() => {
+    dispatch(initTourCards({ pageNumber, cardsCount }));
+  }, [pageNumber, cardsCount]);
 
   return (
-    <section
-      className={`${className} ${style.section}`}
-      style={{
-        marginTop: offsetY,
-      }}
-    >
-      <h2 className="heading-secondary">Tour Packages</h2>
-      <div className={style.content}></div>
+    <section ref={toursSection} className={`${className} ${style.section}`}>
+      <h2 className={`heading-secondary ${style.heading}`}>Tour Packages</h2>
+      <div className={style.content}>
+        {tourCards.map((tour, index) => (
+          <TourCard
+            key={`tour-card_${tour.id}`}
+            id={tour.id}
+            imgAddress={tour.img}
+            title={tour.name}
+            price={tour.price}
+            dateType={tour.scheduleType}
+            minGroupNumber={tour.minNumberOfGroup}
+            maxGroupNumber={tour.maxNumberOfGroup}
+            description={tour.description}
+          ></TourCard>
+        ))}
+      </div>
     </section>
   );
 }
